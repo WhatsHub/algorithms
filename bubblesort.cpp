169f56d21d3f2099c40f3c2ff7f030b9a28d857e
Joschua <joschua.fink@gmx.de>
2017-03-26 15:23:56 +0200

    adds fillArryRand function and printArray function, fixes bubblesort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,22 +1,18 @@
-
+#include <iostream>
 // Bubblesort algorithm
 void bubblesort (int arr[], int length){
 
-  int temp;
+	int temp;
 
-	for(int i=0; i<=length; i++){
-
+	for(int i=0; i < length; i++){
 		// Compare every field with it's successor
-		for(int j=0; j<length; j++){
-
+		for(int j=0; j < length-1; j++){
 			//Swap a number with it's successor if the number is greater
 			if(arr[j]>arr[j+1]){
-				temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;        
+				temp =  arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;        
 			}
-
 		}         
 	} 
-
 }
--- sorting.cpp
+++ sorting.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
 #include "bubblesort.cpp"
 #include "insertionsort.cpp"
+
+using namespace std;
+
+void fillArryRand(int arr[], int length){
+	// initialize random seed
+	srand(time(NULL));
+	// initialize random variable
+	int randomNum = rand() % 100 + 1;
+	// fill array with random numbers between 1 and 100
+	for (int i = 0; i < length; i++){
+		arr[i] = rand() % 100 + 1;
+		cout << "arr[" << i << "] = " << arr[i] << endl;
+	}
+}
+
+void printArray(string name,int arr[], int length){
+	cout << name << " : [";
+	cout << arr[0];	
+	for (int i = 1; i < length; i++){
+		cout << ", "<< arr[i];
+	}
+	cout << "]\n" << endl;
+}
 
 int main(){
 	
@@ -8,21 +34,16 @@ int main(){
 	int arr[] = {8, 5, 3, 6, 9, 2, 1, 4, 7};
 	int length = sizeof(arr)/sizeof(arr[0]);
 
-	// Ausgabe des Arrays
-	for (int i = 0; i < length; i++){
-		std::cout << arr[i];
-	}	
+	string arrname = "myarray";
 
-	// new line
-	std::cout << std::endl;
+	fillArryRand(arr, length);
+
+	printArray("original", arr, length);	
 
 	// Anwendung von insertionsort auf das Array
 	bubblesort(arr, length);
 
-	// Ausgabe des resultierenden Arrays
-	for ( int i = 0; i < length; i++ ){
-		std::cout << arr[i];
-	}
+	printArray("sorted", arr, length);
 
 	return 0;
 }
