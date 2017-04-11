163448267035296f49a38b05d78aa4fbf384c19d
Joschua <joschua.fink@gmx.de>
2017-04-11 20:13:07 +0200

    adds few lines to quicksort algorithm

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,10 +1,21 @@
-
-void partition(int arr[], links, rechts){
-    //TODO
+void swap(int arr[], int a, int b){
+    int tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
 }
 
-void pivot(int arr[], links, rechts){
-    //TODO
+void partition(int arr[], int p, int links, int rechts){
+    //Swappe Pivot und Rechts
+    swap(arr, p, rechts);
+    p = rechts--;            //setze den pivotzeiger auf rechts und dekrementiere rechts danach
+
+    while(links < rechts){
+        //TODO
+    }
+}
+
+void pivot(int arr[], int links, int rechts){
+    return links;
 }
 
 void quicksort (int arr[], int links, int rechts ){
